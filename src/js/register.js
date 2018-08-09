7ee4a05e33475e44467c158b550ae9a3b844080e
Alterna87 <fvelueta@outlook.com>
2018-08-09 18:00:43 -0500

    Add maquetado Canvas

--- src/js/register.js
+++ src/js/register.js
@@ -1,31 +1,49 @@
 let player = document.getElementById('player');
-let snapshotCanvas = document.getElementById('snapshot');
+
 let captureButton = document.getElementById('capture');
 let camera = document.getElementById('camera');
-let frame = document.getElementById('frame');
-
+let recapture = document.getElementById('recapture');
 const handleSuccess = (stream) => {
   player.srcObject = stream;
 };
 
 captureButton.addEventListener('click', ()=> {
-let context = snapshot.getContext('2d');
-context.drawImage(player, 0, 0, snapshotCanvas.width, snapshotCanvas.height);
 
+let shot = document.getElementById('shot');
+shot.innerHTML = `<canvas id='snapshot' width='200' height='180' class = 'col-md offsset-1 camera-radius'></canvas>`;
+snapshotCanvas = document.getElementById('snapshot');
+let context = snapshot.getContext('2d');
+snapshotCanvas.style.display = 'block';
+context.drawImage(player, 0, 0, snapshotCanvas.width, snapshotCanvas.height);
+player.style.display = 'none';
+recapture.style.display = 'block'
+captureButton.style.display = 'none';
 let snap = snapshotCanvas.toDataURL();
 let refImages = firebase.storage().ref();
 
 // console.log(file);
-let uploadImages = refImages.child('images/prueba.png');
-uploadImages.putString(snap, 'data_url').then(function(snapshot) {
-  console.log('Uploaded a base64 string!');
-});
+// let uploadImages = refImages.child(`images/${fecha}.png`);
+// uploadImages.putString(snap, 'data_url').then(function(snapshot) {
+//   console.log('Uploaded a base64 string!');
+// });
 
 });
 
-camera.addEventListener('change', (event)=> {
-  let file = event.target.files[0];
+
+const showsnapshot = () => {
+document.getElementById('data-general').style.display = 'none';
+document.getElementById('picture').style.display = 'block';
+};
+
+recapture.addEventListener('click', ()=> {
+  captureButton.style.display = 'block';
+  recapture.style.display = 'none';
+  player.style.display = 'block';
+  snapshotCanvas.style.display = 'none';
+
 })
+let arrowRight = document.getElementById('arrow-right');
+arrowRight.addEventListener('click', showsnapshot);
  //
  navigator.mediaDevices.getUserMedia({video: true})
   .then(handleSuccess);
--- src/js/welcome.js
+++ src/js/welcome.js
@@ -2,5 +2,7 @@ const begin = () => {
   location.href = ('views/register.html');
 };
 
+
+
 let register = document.getElementById('register');
 register.addEventListener('click', begin);
