246a67c4bae8f8ad712281c09c7d7e13ed91e5b6
Alterna87 <fvelueta@outlook.com>
2018-08-14 09:37:48 -0500

    Add with photo admin

--- src/js/home.js
+++ src/js/home.js
@@ -11,22 +11,30 @@ window.onload = () => {
     .catch(error => console.error('Error:', error))
     // el response ya esta convertido en json por eso se llama difernte
     .then(visitors => {
+
       // transformo el json de objetos en un arreglo de objetos
       const visitorsRows = Object.keys(visitors).reduce((acumulador, visitorId) => {
         // obtiene el visitor por el ID
         const visitor = visitors[visitorId];
+
         // guarda el visitor (objeto) en el arreglo
         acumulador.push(visitor);
         // retorna el arreglo con el nuevo valor
         return acumulador;
       }, []).map((visitor) => {
+
+        let x;
+
+
         // filas que se insertaran en la tabla de visitantes
-        return (`
-          <tr>
-            <td>${visitor.name}</td>
-            <td>${visitor.lastname}</td>
+        return (
+        `   <tr>
+            <td><img src='${visitor.images}' class ='rounded-circle img-thum'></td>
+            <td>${visitor.name} ${visitor.lastname}</td>
             <td>${visitor.empleado}</td>
+            <td>${visitor.time}</td>
             <td>${visitor.date}</td>
+
           </tr>
         `);
       }).join('');
@@ -35,3 +43,12 @@ window.onload = () => {
       return document.getElementById('visitors-table').innerHTML = visitorsRows;
     });
 };
+
+const logOut = ()=> {
+  firebase.auth().signOut();
+  location.href = ('../index.html');
+};
+
+let btnSalir = document.getElementById('exit');
+
+btnSalir.addEventListener('click', logOut);
